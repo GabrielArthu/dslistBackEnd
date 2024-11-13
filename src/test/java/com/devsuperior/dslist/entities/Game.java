dbd42533fecac96ddffc3072189e180f1eb15ddc
Gabriel <gabrielarthur11333@gmail.com>
2024-11-13 09:34:01 -0300

    Config de properties e criacao da classe game

---
+++ src/test/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dslist.entities;
+
+public class Game {
+
+	private long id;
+	private String title;
+	private Integer year;
+	private String genre;
+	private String plataforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+	
+}
